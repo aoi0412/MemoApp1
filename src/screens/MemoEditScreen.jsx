3fd494613be58c97a8b2f19c67afed698c36118d
aoi0412 <aoiaoi0412.1203@gmail.com>
2021-10-23 21:33:38 +0900

    キーボードの修正

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -3,9 +3,11 @@ import { View, StyleSheet, TextInput, KeyboardAvoidingView } from "react-native"
 import App from "../../App";
 import AppBar from "../components/AppBar";
 import CircleButton from "../components/CircleButton";
+import KeyboardSafeView from "../components/KeyboardSafeView";
+
 export default function MemoEditScreen() {
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="height">
+    <KeyboardSafeView style={styles.container}>
       <AppBar />
 
       <View style={styles.inputContainer}>
@@ -13,7 +15,7 @@ export default function MemoEditScreen() {
       </View>
 
       <CircleButton name="check"></CircleButton>
-    </KeyboardAvoidingView>
+    </KeyboardSafeView>
   );
 }
 
